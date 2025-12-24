72975d5a2ebbe146733c800871635010b90ed748
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-12-24 19:59:48 +0100

    Simpelere check voor controleren of identifier matched

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -83,7 +83,7 @@ export class CsvService {
       const lowerCaseIdentifier = identifier.toLowerCase();
 
       // Check if the identifier contains the search term (case insensitive)
-      if (lowerCaseIdentifier === searchTerm || lowerCaseIdentifier.startsWith(searchTerm) || searchTerm.endsWith(lowerCaseIdentifier)) {
+      if (lowerCaseIdentifier === searchTerm || lowerCaseIdentifier.startsWith(searchTerm) || searchTerm.includes(lowerCaseIdentifier)) {
         console.log(`Match found: ${identifier}`);
         matches.set(identifier, data);
         matchCount++;
